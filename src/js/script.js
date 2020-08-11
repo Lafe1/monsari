9b52bdabef205b77c4e4d42be3a2ed4d173c5a6c
Vladimir <sharonov353@gmail.com>
2020-08-11 19:00:26 +0300

    Add nav and header

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	//Tabs
+	$('.header__add-dots').on('click', '.header__add-dot:not(.header__add-dot_active)', function() {
+		$(this)
+		.addClass('header__add-dot_active').siblings().removeClass('header__add-dot_active');
+		// .closest('header').css(
+		// 	"background:", ` url('../img/background${$(this).index()}.jpg') no-repeat`
+		// );
+
+		// find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+});
