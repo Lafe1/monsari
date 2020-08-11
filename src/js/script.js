0cecf6563ea6ae279e908338af3a7c806d277235
Vladimir <sharonov353@gmail.com>
2020-08-11 21:43:29 +0300

    test

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
 	
 	//Tabs
 	$('.header__add-dots').on('click', '.header__add-dot:not(.header__add-dot_active)', function() {
+		console.log($(this).index());
 		$(this)
-		.addClass('header__add-dot_active').siblings().removeClass('header__add-dot_active');
-		// .closest('header').css(
-		// 	"background:", ` url('../img/background${$(this).index()}.jpg') no-repeat`
-		// );
+		.addClass('header__add-dot_active').siblings().removeClass('header__add-dot_active')
+		.closest('header').css(
+			"background:", `url('../img/background${$(this).index()}.jpg') no-repeat`
+		);
 
 		// find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
