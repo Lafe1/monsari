ff16df025585c84a84aea7a302f191aa413e2b9f
Denis <atumif@yandex.r>
2020-08-14 00:27:05 +0300

    gfdgfd

--- src/js/script.js
+++ src/js/script.js
@@ -126,3 +126,19 @@ var slider3 = '.quality-slider__list';
     ]
       
 });
+
+
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn();
+});
+
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+  })
+});
